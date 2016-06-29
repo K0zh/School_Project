3f23135e3f5965b3d2afacddc481249e15fb0d7b
test <Administrator@WIN-FLKBK74O1KM>
2016-06-29 09:37:12 +0900

    맴버추가

--- HeyYo/src/main/java/com/project/heyyo/member/MemberController.java
+++ HeyYo/src/main/java/com/project/heyyo/member/MemberController.java
@@ -1,12 +1,25 @@
 package com.project.heyyo.member;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
+
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+
+
+import com.project.heyyo.member.model.Member;
 import com.project.heyyo.member.model.MemberDao;
 
 @Controller
@@ -22,10 +35,116 @@ public class MemberController {
 		return "SignUpForm";
 	}
 	
-	@RequestMapping(value="user_detail.mb", method=RequestMethod.GET)
-	public String viewUser(@RequestParam("id") int id) {
+	@RequestMapping(value="id_test.mb")
+	public String viewId_testForm(){
+		return "id_Test";
+	}
+	@RequestMapping(value="pw_test.mb")
+	public String viewPw_testForm(){
+		return "pw_Test";
+	}
+	
+	//회원가입 부분
+	@RequestMapping(value="write.mb",method=RequestMethod.POST)
+	public ModelAndView insertSignUp(
+			@ModelAttribute("Member") @Valid Member member, 
+			BindingResult bindingResult){
 		
-		System.out.println(id);
-		return "test";
+		System.out.println(member.getBirthday());
+		ModelAndView mav = new ModelAndView();
+		
+		System.out.println("들어온 이메일:"+member.getEmail());
+		Member mb = memberDao.InquiryEmail(member.getEmail().trim());
+		System.out.println("이메일 증복여부:"+mb);
+		
+
+		if(mb==null){
+			System.out.println("이메일 증복 없음");
+			int insert = memberDao.insert(member);
+			System.out.println("등록 여부:"+insert);
+			mav.setViewName("");
+		}else{
+			System.out.println("이메일 증복됨");
+			mav.setViewName("SignUpForm");
+		}
+		
+		
+		return mav;
+	}
+	
+	//로그인 부분
+	@RequestMapping(value="login.mb",method=RequestMethod.POST)
+	public ModelAndView login(Member member,HttpSession session
+			){
+		System.out.println("로그인 이메일:"+member.getEmail());
+		System.out.println("로그인 비번:"+member.getPw());
+		
+		ModelAndView mav = new ModelAndView();
+		
+		Member mb = memberDao.InquiryEmail(member.getEmail().trim());
+		System.out.println("이메일 증복여부:"+mb);
+		
+		if(mb==null){
+			System.out.println("아이디가 없습니다.");
+			//mav.setViewName("");
+		}else{
+			if(member.getEmail().trim().equals(mb.getEmail())&&
+					member.getPw().trim().equals(mb.getPw())){
+				System.out.println("이메일 비번 맞음.");
+				
+				session.setAttribute("loginfo", mb); 
+				//mav.setViewName("");
+				
+			}else{
+				System.out.println("비밀번호가 틀렸습니다");
+				//mav.setViewName("");
+			}
+		}
+		
+		
+		return mav;
+		
+	}
+	//id 찾기
+	@RequestMapping(value="idInquiry.mb",method=RequestMethod.POST)
+	public ModelAndView idInquiry(
+			@ModelAttribute("Member") @Valid Member member, 
+			BindingResult bindingResult){
+		System.out.println("이름:"+member.getName());
+		System.out.println("전화번호:"+member.getHp());
+		ModelAndView mav = new ModelAndView();
+		
+		Member mb = memberDao.email_test(member);
+		if(mb==null){
+			System.out.println("일치하는 이메일이 없습니다.");
+			//mav.setViewName("");
+		}else{
+			System.out.println("찾은 이름:"+mb.getEmail());
+			mav.addObject("mb", mb);
+			//mav.setViewName("test");
+		}
+		return mav;
+	}
+	
+	//pw 찾기
+	@RequestMapping(value="pwInquiry.mb",method=RequestMethod.POST)
+	public ModelAndView pwInquiry(
+			@ModelAttribute("Member") @Valid Member member, 
+			BindingResult bindingResult){
+		System.out.println("이메일:"+member.getEmail());
+		System.out.println("이름:"+member.getName());
+		System.out.println("전화번호:"+member.getHp());
+		ModelAndView mav = new ModelAndView();
+		
+		Member mb = memberDao.pw_test(member);
+		if(mb==null){
+			System.out.println("일치하는 이메일이 없습니다.");
+			//mav.setViewName("");
+		}else{
+			System.out.println("찾은 비번:"+mb.getPw());
+			mav.addObject("mb", mb);
+			//mav.setViewName("test");
+		}
+		return mav;
 	}
 }
--- HeyYo/src/main/java/com/project/heyyo/member/model/Member.java
+++ HeyYo/src/main/java/com/project/heyyo/member/model/Member.java
@@ -1,136 +1,136 @@
 package com.project.heyyo.member.model;
 
 public class Member {
+		//테이블 목록
+		private int num;
+		private String email;
+		private String pw;
+		private String name;
+		private String gender;
+		private int hp;
+		private String address;
+		private String talentDt;
+		private String talentGet;
+		private String image;
+		private int M_like;
+		private String R_date;
+		private String hpopen;
+		
+		private String birthday;
+		
+		
+			public Member() {
+				super();
+			}
+			public Member(int num, String email, String pw, String name, String gender, int hp,
+					String address, String talentDt, String talentGet, String image, int M_like, String R_date, String hpopen, String birthday){
+				this.num=num;
+				this.email=email;
+				this.pw=pw;
+				this.name=name;
+				this.gender=gender;
+				this.hp=hp;
+				this.address=address;
+				this.talentDt=talentDt;
+				this.talentGet=talentGet;
+				this.image=image;
+				this.M_like=M_like;
+				this.R_date=R_date;
+				this.hpopen=hpopen;
+				this.birthday=birthday;
+			}
+			
+			
+		
+		public String getBirthday() {
+				return birthday;
+			}
+			public void setBirthday(String birthday) {
+				this.birthday = birthday;
+			}
+		public String getHpopen() {
+				return hpopen;
+			}
+			public void setHpopen(String hpopen) {
+				this.hpopen = hpopen;
+			}
+		
+			
+			
+			
+		public int getNum() {
+			return num;
+		}
+		public void setNum(int num) {
+			this.num = num;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPw() {
+			return pw;
+		}
+		public void setPw(String pw) {
+			this.pw = pw;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public int getHp() {
+			return hp;
+		}
+		public void setHp(int hp) {
+			this.hp = hp;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getTalentDt() {
+			return talentDt;
+		}
+		public void setTalentDt(String talentDt) {
+			this.talentDt = talentDt;
+		}
+		public String getTalentGet() {
+			return talentGet;
+		}
+		public void setTalentGet(String talentGet) {
+			this.talentGet = talentGet;
+		}
+		public String getImage() {
+			return image;
+		}
+		public void setImage(String image) {
+			this.image = image;
+		}
+		public int getM_like() {
+			return M_like;
+		}
+		public void setM_like(int m_like) {
+			M_like = m_like;
+		}
+		public String getR_date() {
+			return R_date;
+		}
+		public void setR_date(String r_date) {
+			R_date = r_date;
+		}
+	
 
-	private int id;
-	private String email;
-	private String password;
-	private String name;
-	private String gender;
-	private String hp;
-	private String address;
-	private String talentDt;
-	private String talentGet;
-	private String image;
-	private int like;
-	private String r_date;
-
-	public Member() {
-		super();
-	}
-
-	public Member(int id, String email, String password, String name,
-			String gender, String hp, String address, String talentDt,
-			String talentGet, String image, int like, String r_date) {
-		super();
-		this.id = id;
-		this.email = email;
-		this.password = password;
-		this.name = name;
-		this.gender = gender;
-		this.hp = hp;
-		this.address = address;
-		this.talentDt = talentDt;
-		this.talentGet = talentGet;
-		this.image = image;
-		this.like = like;
-		this.r_date = r_date;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getHp() {
-		return hp;
-	}
-
-	public void setHp(String hp) {
-		this.hp = hp;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getTalentDt() {
-		return talentDt;
-	}
-
-	public void setTalentDt(String talentDt) {
-		this.talentDt = talentDt;
-	}
-
-	public String getTalentGet() {
-		return talentGet;
-	}
-
-	public void setTalentGet(String talentGet) {
-		this.talentGet = talentGet;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	public int getLike() {
-		return like;
-	}
-
-	public void setLike(int like) {
-		this.like = like;
-	}
-
-	public String getR_date() {
-		return r_date;
-	}
-
-	public void setR_date(String r_date) {
-		this.r_date = r_date;
-	}
-
+	
 }
